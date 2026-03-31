c1dce4e434df5e76931c9751c68cbfca8dcc234e
amine <aminear220@gmail.com>
2026-03-31 16:00:40 +0100

    set up react project

---
+++ src/Todo/Todo.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function App() {
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleAdd = () => {
+    setList([...list, text]); 
+    setText(""); 
+  };
+
+  return (
+    <div>
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button onClick={handleAdd}>add</button>
+
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
