a1b429b0b9c3a579f9f2c6f2e251c8ea2b04cce1
amine <aminear220@gmail.com>
2026-03-31 16:07:09 +0100

    feat : added folder todo list and add input and button with list display

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -1,29 +1,29 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// function App() {
-//   const [text, setText] = useState("");
-//   const [list, setList] = useState([]);
+function App() {
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
 
-//   const handleAdd = () => {
-//     setList([...list, text]); 
-//     setText(""); 
-//   };
+  const handleAdd = () => {
+    setList([...list, text]); 
+    setText(""); 
+  };
 
-//   return (
-//     <div>
-//       <input
-//         value={text}
-//         onChange={(e) => setText(e.target.value)}
-//       />
+  return (
+    <div>
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
 
-//       <button onClick={handleAdd}>add</button>
+      <button onClick={handleAdd}>add</button>
 
-//       <ul>
-//         {list.map((item, index) => (
-//           <li key={index}>{item}</li>))}
-//       </ul>
-//     </div>
-//   );
-// }
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>))}
+      </ul>
+    </div>
+  );
+}
 
  export default App;
