ff57133a0f523ea1f39069aea7291d7590ecd706
amine <aminear220@gmail.com>
2026-04-03 15:21:05 +0100

    feat: add function to create new todo item

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -1,29 +1,67 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import "./todo.css";
 
-function App() {
-  const [text, setText] = useState("");
-  const [list, setList] = useState([]);
+function Todo() {
+    const [todos, setTodos] = useState([]);
+    const inputRef = useRef();
+    const error = "The input is empty";
 
-  const handleAdd = () => {
-    setList([...list, text]); 
-    setText(""); 
-  };
+    
+    const addTodo = () => {
+        const text = inputRef.current.value;
 
-  return (
-    <div>
-      <input
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
+        if (text.trim() !== "") {
+            const newTodo = {
+                text: text,
+                completed: false
+            };
 
-      <button onClick={handleAdd}>add</button>
+            setTodos([...todos, newTodo]);
+        } else {
+            alert(error);
+        }
 
-      <ul>
-        {list.map((item, index) => (
-          <li key={index}>{item}</li>))}
-      </ul>
-    </div>
-  );
+        inputRef.current.value = "";
+    };
+
+   
+    // const toggleComplete = (index) => {
+    //     const newTodos = [...todos];
+    //     newTodos[index].completed = !newTodos[index].completed;
+    //     setTodos(newTodos);
+    // };
+
+    
+    // const deleteTodo = (index) => {
+    //     const newTodos = todos.filter((_, i) => i !== index);
+    //     setTodos(newTodos);
+    // };
+
+    return (
+        <div className="todo">
+            <h1>Todo List Project</h1>
+            <ul>
+                {todos.map(({ text, completed }, index) => {
+                    return (
+                        <li key={index} className={completed ? "done" : ""}>
+                            
+                            <span onClick={() => toggleComplete(index)}>
+                                {text}
+                            </span>
+
+                            <button onClick={() => deleteTodo(index)}>
+                                
+                            </button>
+
+                        </li>
+                    );
+                })}
+            </ul>
+
+            <input type="text" ref={inputRef} />
+            <button onClick={addTodo}>ADD</button>
+        </div>
+    );
 }
 
- export default App;
+export default Todo;
